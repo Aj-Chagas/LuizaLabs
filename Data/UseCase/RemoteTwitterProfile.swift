afb0915eb11cb0fc7b8376bb4713e9419f2787ca
Anderson Chagas <anderson.1997reis@gmail.com>
2021-08-07 13:19:29 -0300

    reecriando o target data layer

---
+++ Data/UseCase/RemoteTwitterProfile.swift
@@ -0,0 +1,18 @@
+//
+//  RemoteTwitterProfile.swift
+//  Data
+//
+//  Created by Anderson Chagas on 07/08/21.
+//
+
+import Foundation
+import Domain
+
+class RemoteTwitterProfile: TwitterProfile {
+
+    func fetchTwitterProfile(fetchTwitterProfileModel: FetchTwitterProfileModel,
+                             completion: @escaping (TwitterProfile.Result) -> Void) {
+        
+    }
+    
+}
---
+++ DataTests/UseCase/RemoteTwitterProfileTests.swift
@@ -0,0 +1,17 @@
+//
+//  DataTests.swift
+//  DataTests
+//
+//  Created by Anderson Chagas on 07/08/21.
+//
+
+import XCTest
+@testable import Data
+
+class RemoteTwitterProfileTests: XCTestCase {
+
+    func test_() {
+        
+    }
+
+}
