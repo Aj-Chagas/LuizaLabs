59abbee4786ccced249f09f8784e11c944e9e6d2
Anderson Chagas <anderson.1997reis@gmail.com>
2021-08-07 16:09:22 -0300

    add new target infra layer

---
+++ Infra/Infra.swift
@@ -0,0 +1,12 @@
+//
+//  Infra.swift
+//  Infra
+//
+//  Created by Anderson Chagas on 07/08/21.
+//
+
+import Foundation
+
+public final class Infra {
+    
+}
---
+++ InfraTests/InfraTests.swift
@@ -0,0 +1,16 @@
+//
+//  InfraTests.swift
+//  InfraTests
+//
+//  Created by Anderson Chagas on 07/08/21.
+//
+
+import XCTest
+import Infra
+
+class InfraTests: XCTestCase {
+
+    func test_() {
+    }
+
+}
