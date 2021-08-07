1daf9e6d8ef0388ea3a167c80cb354d245363890
Anderson Chagas <anderson.1997reis@gmail.com>
2021-08-07 12:43:53 -0300

    create a new target Data Layer

---
+++ DataTests/DataTests.swift
@@ -0,0 +1,17 @@
+//
+//  DataTests.swift
+//  DataTests
+//
+//  Created by Anderson Chagas on 07/08/21.
+//
+
+import XCTest
+@testable import Data
+
+class DataTests: XCTestCase {
+
+    func test_() {
+        
+    }
+
+}
