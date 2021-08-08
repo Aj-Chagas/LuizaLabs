ec3f67e10eaff659769dccabdf91bee9fe0efcb0
Anderson Chagas <anderson.1997reis@gmail.com>
2021-08-07 22:00:01 -0300

    move HttpGetClientSpy to its own file

--- DataTests/UseCase/RemoteTwitterProfileTests.swift
+++ DataTests/UseCase/RemoteTwitterProfileTests.swift
@@ -90,29 +90,3 @@ extension RemoteTwitterProfileTests {
     }
     
 }
-    
-
-class HttpGetClientSpy: HttpGetClient {
-
-    var urls = [URL]()
-    var completion: ((Result<Data?, HttpError>) -> Void)?
-    var params: [String : Any]?
-    var headers: [String : String]?
-
-    func get(to url: URL, params: [String : Any]?, headers: [String : String]?, completion: @escaping (Result<Data?, HttpError>) -> Void) {
-        self.urls.append(url)
-        self.params = params
-        self.completion = completion
-        self.params = params
-        self.headers = headers
-    }
-
-    func completionWithError(_ httpError: HttpError = .noConnectivity) {
-        completion?(.failure(httpError))
-    }
-
-    func completionWithSuccess(_ data: Data?) {
-        completion?(.success(data))
-    }
-
-}
