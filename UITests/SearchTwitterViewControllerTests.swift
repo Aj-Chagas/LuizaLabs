0904d3ec8a5c9a47d9c7f0d22da8e9185635f5a3
Anderson Chagas <anderson.1997reis@gmail.com>
2021-08-10 22:05:19 -0300

    add UILayer

---
+++ UITests/SearchTwitterViewControllerTests.swift
@@ -0,0 +1,16 @@
+//
+//  SearchTwitterViewControllerTests.swift
+//  UITests
+//
+//  Created by Anderson Chagas on 10/08/21.
+//
+
+import XCTest
+import UI
+
+class SearchTwitterViewControllerTests: XCTestCase {
+
+    func test_() {
+        
+    }
+}
