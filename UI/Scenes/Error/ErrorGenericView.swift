bedf81879462589c583c45f493b7a33203a2335e
Anderson Chagas <anderson.1997reis@gmail.com>
2021-08-11 13:29:07 -0300

    create layout errorGeneric

---
+++ UI/Scenes/Error/ErrorGenericView.swift
@@ -0,0 +1,16 @@
+//
+//  ErrorGenericView.swift
+//  UI
+//
+//  Created by Anderson Chagas on 11/08/21.
+//
+
+import UIKit
+
+class ErrorGenericView: UIView {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var subTitle: UILabel!
+    @IBOutlet weak var backButton: UIButton!
+}
---
+++ UI/Scenes/Error/ErrorGenericViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ErrorGenericViewController.swift
+//  UI
+//
+//  Created by Anderson Chagas on 11/08/21.
+//
+
+import UIKit
+
+class ErrorGenericViewController: UIViewController, Storyboarded, ControllerWithMainView {
+    
+    typealias MainView = ErrorGenericView
+    
+}
