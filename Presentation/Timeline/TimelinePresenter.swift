e88a6b6392c5eb97974da6eba9c05d8d2927def5
Anderson Chagas <anderson.1997reis@gmail.com>
2021-08-10 21:31:37 -0300

    ensure when calls analyzeSentiment if completes with failure timeLinePresenter calls showErroScreen

--- Presentation/Timeline/TimelinePresenter.swift
+++ Presentation/Timeline/TimelinePresenter.swift
@@ -8,14 +8,26 @@
 import Foundation
 import Domain
 
+public protocol TimelineDelegate: AnyObject {
+    func showErrorScreen()
+}
+
 public final class TimelinePresenter {
     private let analyzeSentiment: AnalyzeSentiment
+    private let delegate: TimelineDelegate
     
-    public init(analyzeSentiment: AnalyzeSentiment) {
+    public init(analyzeSentiment: AnalyzeSentiment,
+                delegate: TimelineDelegate) {
         self.analyzeSentiment = analyzeSentiment
+        self.delegate = delegate
     }
     
     public func analyzeSentiment(with model: FetchAnalyzeSentimentModel) {
-        analyzeSentiment.fetchAnalyzeSentiment(fetchTweetTimeLineModel: model) { _ in }
+        analyzeSentiment.fetchAnalyzeSentiment(fetchTweetTimeLineModel: model) { result in
+            switch result {
+            case .success: break
+            case .failure: self.delegate.showErrorScreen()
+            }
+        }
     }
 }
--- PresentationTests/TimelinePresenter/TimelinePresenterTests.swift
+++ PresentationTests/TimelinePresenter/TimelinePresenterTests.swift
@@ -10,7 +10,7 @@ import Domain
 import Presentation
 
 class TimelinePresenterTests: XCTestCase {
-    func test_searchTwitter_should_call_twitter_profile_with_correct_values() {
+    func test_analyzeSentiment_should_call_twitter_profile_with_correct_values() {
         let analyzeSentiment = AnalyzeSentimentSpy()
         let model = makeFetchAnalyzeSentimentModel()
         let sut = makeSut(analyzeSentiment: analyzeSentiment)
@@ -19,25 +19,52 @@ class TimelinePresenterTests: XCTestCase {
         
         XCTAssertEqual(analyzeSentiment.model, model)
     }
+
+    func test_analyzeSentiment_should_call_showErrorScreen_when_analyzeSentiment_completes_with_failure() {
+        let analyzeSentiment = AnalyzeSentimentSpy()
+        let delegate = TimelineDelegateSpy()
+        let model = makeFetchAnalyzeSentimentModel()
+        let sut = makeSut(analyzeSentiment: analyzeSentiment, delegate: delegate)
+        
+        sut.analyzeSentiment(with: model)
+        
+        analyzeSentiment.completionWithError()
+        
+        XCTAssert(delegate.screenError)
+    }
 }
 
 extension TimelinePresenterTests {
-    func makeSut(analyzeSentiment: AnalyzeSentiment = AnalyzeSentimentSpy()) -> TimelinePresenter {
-        return TimelinePresenter(analyzeSentiment: analyzeSentiment)
+    func makeSut(analyzeSentiment: AnalyzeSentiment = AnalyzeSentimentSpy(),
+                 delegate: TimelineDelegate = TimelineDelegateSpy()) -> TimelinePresenter {
+        return TimelinePresenter(analyzeSentiment: analyzeSentiment, delegate: delegate)
     }
 }
 
 class AnalyzeSentimentSpy: AnalyzeSentiment {
-    
+
     var model: FetchAnalyzeSentimentModel?
     var completion: ((AnalyzeSentiment.Result) -> Void)?
-    
-    
+
     func fetchAnalyzeSentiment(fetchTweetTimeLineModel model: FetchAnalyzeSentimentModel, completion: @escaping (AnalyzeSentiment.Result) -> Void) {
         self.model = model
         self.completion = completion
     }
-    
+
+    func completionWithError(_ error: DomainError = .unexpected) {
+        completion?(.failure(.unexpected))
+    }
+
+}
+
+class TimelineDelegateSpy: TimelineDelegate {
+
+    var screenError: Bool = false
+
+    func showErrorScreen() {
+        screenError = true
+    }
+
 }
 
 
