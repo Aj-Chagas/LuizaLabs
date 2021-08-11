44022bff9f7cf59b8835d9e487fa5475db3a95dd
Anderson Chagas <anderson.1997reis@gmail.com>
2021-08-10 23:51:29 -0300

    connect ibottles

--- UI/Scenes/SearchTwitterView.swift
+++ UI/Scenes/SearchTwitterView.swift
@@ -10,5 +10,11 @@ import SnapKit
 
 class SearchTwitterView: UIView {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var dividerView: UIView!
+    @IBOutlet weak var errorView: UIView!
+    @IBOutlet weak var errorIcon: UIImageView!
+    @IBOutlet weak var errorMessageLabel: UILabel!
     
 }
