bf2c18f8ae9b39e32a9a526f2de0135e18b54c87
Anderson Chagas <anderson.1997reis@gmail.com>
2021-08-11 22:24:09 -0300

    create layout and view controller to ConclusionViewController

---
+++ UI/Scenes/Conclusion/ConclusionView.swift
@@ -0,0 +1,17 @@
+//
+//  ConclusionView.swift
+//  UI
+//
+//  Created by Anderson Chagas on 11/08/21.
+//
+
+import UIKit
+
+class ConclusionView: UIView {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var tweetLabel: UILabel!
+    @IBOutlet weak var secondTitle: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var resultIcon: UIImageView!
+}
---
+++ UI/Scenes/Conclusion/ConclusionViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ConclusionViewController.swift
+//  UI
+//
+//  Created by Anderson Chagas on 11/08/21.
+//
+
+import UIKit
+import Presentation
+
+public final class ConclusionViewController: UIViewController, ControllerWithMainView, Storyboarded {
+
+    typealias MainView = ConclusionView
+    
+}
